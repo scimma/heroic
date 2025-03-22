3a0628c52e5f417b3393e712a49eca563014ef1d
Jon <jnation@lco.global>
2025-03-22 03:42:55 -0100

    Add drf-spectacular for openAPI docs and document the visibility APIs better

--- heroic_api/views.py
+++ heroic_api/views.py
@@ -3,10 +3,13 @@ from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
 
 from django.contrib.auth.models import User
 
-from heroic_api.serializers import ProfileSerializer, TargetVisibilityQuerySerializer
+from heroic_api.serializers import (ProfileSerializer, TargetVisibilityQuerySerializer,
+                                    TargetVisibilityIntervalResponseSerializer,
+                                    TargetVisibilityAirmassResponseSerializer)
 from heroic_api.visibility import get_rise_set_intervals_by_telescope_for_target, get_airmass_by_telescope_for_target
 
 
@@ -28,6 +31,12 @@ class TargetVisibilityAPIView(APIView):
     """ A API view to get visiblity intervals for targets on telescopes
         Supports being called through POST with a data dict or GET with query params
     """
+    serializer_class = TargetVisibilityQuerySerializer
+    example_response = {
+        'telescope_id': [['2025-03-01T16:15:00Z', '2025-03-01T19:00:37.291560Z'], ['2025-03-01T16:15:00Z', '2025-03-01T19:00:37.291560Z']],
+        'telescope2_id': [['2025-03-01T16:15:00Z', '2025-03-01T19:00:37.291560Z']]
+    }
+
     def get_visibility(self, data):
         serializer = TargetVisibilityQuerySerializer(data=data)
         if serializer.is_valid():
@@ -37,9 +46,31 @@ class TargetVisibilityAPIView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(
+        operation_id='query visibility intervals',
+        parameters=[TargetVisibilityQuerySerializer],
+        responses={
+            200: OpenApiResponse(
+                response=TargetVisibilityIntervalResponseSerializer,
+                examples=[OpenApiExample(name='Success',
+                    value=example_response
+                )]
+           )
+        }
+    )
     def get(self, request):
         return self.get_visibility(request.query_params)
 
+    @extend_schema(
+        operation_id='query visibility intervals (post)',
+        responses={
+            200: OpenApiResponse(
+                response=TargetVisibilityIntervalResponseSerializer,
+                examples=[OpenApiExample(name='Success',
+                    value=example_response
+                )]
+           )
+        })
     def post(self, request):
         return self.get_visibility(request.data)
 
@@ -48,6 +79,16 @@ class TargetAirmassAPIView(APIView):
     """ A API view to get airmasses for targets on telescopes at times
         Supports being called through POST with a data dict or GET with query params
     """
+    serializer_class = TargetVisibilityQuerySerializer
+    example_response = {
+        'telescope_id': {'times': ['2025-03-01T16:15:00Z', '2025-03-01T16:25:00.00Z', '2025-03-01T16:35:00.00Z'],
+                         'airmasses': [1.2342, 1.34543, 1.4564]
+                        },
+        'telescope2_id': {'times': ['2025-03-01T16:15:00Z', '2025-03-01T16:25:00.00Z', '2025-03-01T16:35:00.00Z'],
+                         'airmasses': [1.2342, 1.34543, 1.4564]
+                        },
+        }
+
     def get_airmass(self, data):
         serializer = TargetVisibilityQuerySerializer(data=data)
         if serializer.is_valid():
@@ -57,8 +98,31 @@ class TargetAirmassAPIView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(
+        operation_id='query airmass values',
+        parameters=[TargetVisibilityQuerySerializer],
+        responses={
+            200: OpenApiResponse(
+                response=TargetVisibilityAirmassResponseSerializer,
+                examples=[OpenApiExample(name='Success',
+                    value=example_response
+                )]
+           )
+        }
+    )
     def get(self, request):
         return self.get_airmass(request.query_params)
 
+    @extend_schema(
+        operation_id='query airmass values (post)',
+        responses={
+            200: OpenApiResponse(
+                response=TargetVisibilityAirmassResponseSerializer,
+                examples=[OpenApiExample(name='Success',
+                    value=example_response
+                )]
+           )
+        }
+    )
     def post(self, request):
         return self.get_airmass(request.data)
