10881f0bbf21ec556709efac9170dec4f151c672
Jon <jnation@lco.global>
2026-07-28 23:38:43 +0000

    Fix parsing error

--- heroic_api/tasks.py
+++ heroic_api/tasks.py
@@ -89,7 +89,7 @@ def poll_rubin_schedule():
     
     # Get the schedule from 15 minutes in the past until 25 hours later
     start = datetime.now() - timedelta(minutes=15)
-    logger.info(f'Getting the Rubin schedule starting at {start.strftime('%Y-%m-%d %H:%M:%S')}')
+    logger.info(f'Getting the Rubin schedule starting at {start.strftime("%Y-%m-%d %H:%M:%S")}')
     params = {'time': '25', 'start': start.strftime('%Y-%m-%d %H:%M:%S')}
     response = requests.get(settings.RUBIN_SCHEDULE_URL, params=params, timeout=RUBIN_REQUEST_TIMEOUT)
     response.raise_for_status()
